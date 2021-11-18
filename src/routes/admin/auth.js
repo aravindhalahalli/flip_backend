c3cafc5194ab635771f1961408a51584f5e40430
Aravind Kumar <aravind@byteally.com>
2021-11-18 13:15:15 +0530

    admin signout api created

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { sign } = require("jsonwebtoken");
-const { sigup, signin } = require("../../controller/admin/auth");
+const { requireSignin } = require("../../common-middleware");
+const { sigup, signin, signout } = require("../../controller/admin/auth");
 const { validationSignUpRequest, isRequestValidated, validationSignInRequest } = require("../../validators/auth");
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 //userLogin/Register-Routes
 router.post("/admin/signup", validationSignUpRequest, isRequestValidated, sigup)
 router.post("/admin/signin", validationSignInRequest, isRequestValidated, signin)
+router.post("/admin/signout",requireSignin,signout)
 
 module.exports = router;
